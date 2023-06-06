27efe9435fc3c9f10f3c248aa0f8dea952f3ced9
leticiamontilha <leticiammontilha@hotmail.com>
2023-06-06 17:12:28 -0300

    feat: create subHeader

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,16 @@
 import { CardAd } from "../../components/Card";
 import StyledHome from "./style";
 
+
 const Home = () => {
   return (
     <StyledHome>
       <h1> Home page </h1>
+      
+      <div className="subHeader">
+        <h2>Motors Shop</h2>
+        <p>A melhor plataforma de anúncios de carros do país</p>
+      </div>
 
       <section>
         <ul>
