8047cea82e30e59599af7e21203a1f79bf5e8069
Paola <pa_rosa@msn.com>
2023-06-13 17:25:43 -0300

    feat:register style desktop and mobile

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import ProductDetailsPage from "../pages/ProductDetails";
 import Home from "../pages/home";
+import Register from "../pages/register";
 
 const RoutesMain = () => {
   return (
     <Routes>
+      <Route path="/register" element={<Register />} />
       <Route path="/" element={<Home />} />
       <Route path="/announcement" element={<ProductDetailsPage />} />
     </Routes>
