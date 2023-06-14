80ae2093efd41415e5fce7fe853ee6ef527803db
Glayson Cardoso <glaysoncardoso7@gmail.com>
2023-06-14 15:27:23 -0300

    feat: login

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import ProductDetailsPage from "../pages/ProductDetails";
 import Home from "../pages/home";
+import Signin from "../pages/signin";
 
 const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/announcement" element={<ProductDetailsPage />} />
+      <Route path="/signin" element={<Signin/>} />
     </Routes>
   );
 };
